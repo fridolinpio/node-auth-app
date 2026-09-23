48a6a44d2966ea23e54d439194c4ed79836b64e4
pardhivar <pardhivmittu2003@gmail.com>
2026-09-23 11:33:37 +0530

    Add signup feature

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const signupMessage = require('./signup');
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,10 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Auth App is running');
+});
+
+app.get('/signup', (req, res) => {
+    res.send(signupMessage);
 });
 
 app.listen(PORT, () => {
